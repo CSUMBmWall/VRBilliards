6379d3bd3f4316433e87d22e5a9156389973edd2
matt <mwall@csumb.edu>
2015-11-30 22:04:22 -0800

    finished intro Flyby.  Added flyby script that doesn't work

---
+++ Assets/PlayIntroFlyBy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayIntroFlyBy : MonoBehaviour {
+	public Animation animation;
+
+	// Use this for initialization
+	void Start () {
+		animation = Resources.Load("introFlyBy");
+		animation.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
